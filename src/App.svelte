6922fc9344f24fcc5524356ff31ff4ca20cfd30e
Kartik Rao <kr40.git@gmail.com>
2021-12-15 12:33:15 +0530

    added comments

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,23 @@
+<!-- JS goes here -->
 <script>
 	import logo from './assets/svelte.png';
 	import Toggle from './lib/Toggle.svelte';
 </script>
 
+<!-- Markup goes here -->
 <main class="container">
 	<Toggle />
-
 	<img src={logo} alt="Svelte Logo" />
 	<h1>Svelte Playground</h1>
 	<p>
 		This is a svelte template scaffolded with <strong>vitejs</strong> & comes with
 		<strong>sanitize.css, water.css, font-awesome 5, stylelint & eslint</strong> pre configured.
 		<br /><br />
-
 		<strong> <span><i class="fas fa-info-circle" /></span>&nbsp; Assumes Prettier is already configured.</strong>
 	</p>
 </main>
 
+<!-- CSS goes here -->
 <style>
 	.container {
 		display: flex;
